79b19801078a91422a8d41706fbe9307844cee7f
yuzoolcode <michaelfrankland@Michael-Franklands-iMac-3.local>
2015-04-02 10:42:40 +0900

    Lang copy changes

--- a/config/language/English.php
+++ a/config/language/English.php
@@ -1,10 +1,4 @@
 <?php
-/*
-Name: Droppy - Online file sharing (Translation file)
-Lanuage: English
-Translated by: Proxibolt
-File: English.php
-*/
 
 //Turning off error reporting
 error_reporting(0);
@@ -20,42 +14,42 @@ $text = array(
     'files' 					=> 'Files',
     'link' 						=> 'Link',
     'email' 					=> 'E-Mail',
-    'success' 					=> 'Success !',
+    'success' 					=> 'Success!',
     'password'        			=> 'Password',
     'message' 					=> 'Message',
     'download' 					=> 'Download',
-    'destruct' 					=> 'Destruct',
+    'destruct' 					=> 'Destroy',
     'select_files' 				=> 'Select file(s)',
     'add_more' 					=> 'Add more',
     'total_size' 				=> 'Total size',
     'total_files'				=> 'Total files',
     'download_id' 				=> 'Download id',
     'add_password' 				=> 'Add password',
-    'upload_error' 				=> 'Upload error !',
-    'wrong_pass' 				=> 'Invalid password !',
+    'upload_error' 				=> 'Upload error!',
+    'wrong_pass' 				=> 'Invalid password!',
     'enter_email' 				=> 'Enter email to send to',
     'enter_own_email' 			=> 'Enter your email',
-    'fill_fields' 				=> 'Fill in all fields !',
+    'fill_fields' 				=> 'Fill in all fields!',
     'message_receiver' 			=> 'Message to the receiver',
     'download_started' 			=> 'Download has been started...',
     'enable_destuct' 			=> 'Enable self destruct',
     'file_to_large' 			=> 'The file(s) are too large.',
     'fill_password' 			=> 'Please fill in the password below',
-    'download_started' 			=> 'Processing your download, please be patient',
-    'upload_not_found' 			=> 'File(s) not found, maybe it have been destroyed ?',
-    'processing_files' 			=> 'Processing file(s) and generating link, please be patient.',
+    'download_started' 			=> 'Processing your download, thanks for using Ladybug',
+    'upload_not_found' 			=> 'File(s) not found, maybe it have been destroyed?',
+    'processing_files' 			=> 'Processing file(s) and generating link, hang on a second..',
     'success_link' 				=> 'Your file(s) have been uploaded, you can use the link below.',
     'success_email' 			=> 'Your file(s) have been uploaded, you can expect an email shortly.',
 
     //Added in update 1.0.2
     'select_share' => 'Select how to share:',
-    'file_blocked' => 'These file(s) are not allowed !',
+    'file_blocked' => 'These file(s) are not allowed!',
 
     //Added in update 1.0.4
     'report_file' => 'Report file',
-    'report_file_text' => 'Are you sure you want to report this file ?',
+    'report_file_text' => 'Are you sure you want to report this file?',
     'report' => 'Report',
-    'max_files' => 'To many files selected !'
+    'max_files' => 'To many files selected!'
 );
 
 ?>
